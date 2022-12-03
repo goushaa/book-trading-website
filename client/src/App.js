30891e58be2bdfb82e1c1f5cf0225d4716c3f3ef
goushaa <93336002+goushaa@users.noreply.github.com>
2022-12-03 12:50:49 +0200

    l

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React, { Fragment } from "react";
 import NavbarClient from './components/NavbarClient';
-
+import Mohamed from './components/Mohamed';
 
 function App() {
   return (
     <Fragment>
       <NavbarClient />
+      <Mohamed />
       <h1>Client Side</h1>
     </Fragment>
   );
---
+++ client/src/components/Mohamed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mohamed() {
+    return (
+        <div>
+            <h1>Mohamed</h1>
+        </div>
+    )
+}
+
+export default Mohamed
