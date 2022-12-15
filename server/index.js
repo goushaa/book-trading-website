ad855f403997d8aaf1d1e11fb4f0f84811b8d418
goushaa <93336002+goushaa@users.noreply.github.com>
2022-12-15 20:05:38 +0200

    backend info

--- server/index.js
+++ server/index.js
@@ -9,3 +9,36 @@ app.use(express.json());
 app.listen(5000, () => {
     console.log("server has started on port 5000");
 })
+
+//routes
+
+//signup & login
+//create app.post (validation if exist) sign up
+//if driver open other page
+//update extra info app.post
+//create app.post (validation if exist & send type) sign in
+
+//retrieve all books app.get
+
+//stores add books app.post
+//stores delete books app.delete
+
+//user add book app.post
+//user delete bidItem app.delete
+//user adds wishlist app.post
+//user views wishlists app.get
+//user adds bid app.post
+//users view all bids app.get
+//user bids app.post
+
+//admin adds coupon app.post
+//admin sees users wishlist app.get
+//admin assigns orders to drivers app.post
+//admin views all drivers info app.get
+//admin views all users info app.get
+
+//driver views pending assigned order app.get
+//driver finishes order app.post
+
+
+
