4f03c9c6036d785b6171c7d378b38581b9af5155
3abqreno <abdelrahman.abdelfatah35@gmail.com>
2022-12-17 16:02:28 +0200

    firstCommit

---
+++ insertData.sql
@@ -0,0 +1,2 @@
+insert into city ("name") values ('Giza'),('Cairo'),('Alexandria'),('Aswan'),('Luxor'),('Damietta'),('Suez'),('Ismailia'),('Port Said'),('Asyut'),('Qena'),('Sohag'),('Kafr El Sheikh'),('Beni Suef'),('North Sinai'),('Beheira'),('Minya'),('Faiyum'),('Qalyubia'),('Tanta'),('Zagazig');
+/* city insert*/
