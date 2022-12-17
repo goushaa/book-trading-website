2b3e5df303f1d0b98f65fa18e39bcdf611679c61
goushaa <93336002+goushaa@users.noreply.github.com>
2022-12-17 17:34:36 +0200

    merge3allam and some views

--- server/index.js
+++ server/index.js
@@ -19,6 +19,17 @@ app.listen(5000, () => {
 //update extra info app.put
 //create app.post (validation if exist & send type) sign in
 
+
+// signup
+app.post("/signup", async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+
 //retrieve all books app.get
 
 //stores add books app.post
@@ -32,28 +43,58 @@ app.listen(5000, () => {
 //users view all bids app.get
 //user bids app.post
 
-//admin adds coupon app.post
-//admin sees users wishlist app.get
-//admin assigns orders to drivers app.post
-//admin views all drivers info app.get
-//admin views all users info app.get
-
 //driver views pending assigned order app.get
 //driver finishes order app.post
 
-//coupons
+//admin adds coupon app.post (DONE)
+//admin sees user's wishlist app.get (NEEDS REVISION)
+//admin assigns orders to drivers app.post
 
+//(NEEDS REVISION WHEN SIGNIN IN DONE & (DRIVER,users,bookstores) IS ADDED TO THE SYSTEM)
+//admin views all drivers info app.get 
+//admin views all users info app.get
+//admin views all stores info app.get
+
+//coupons
 app.post("/addCoupon", async (req, res) => {
     try {
-        const { code, discount, maximumUse, isRelative } = req.body;
+        //validated in backend
         //code not repeated
-        //discount positive
-        //maximum positive  
-        //isRelative 0,1
-        const newCoupon = await pool.query("INSERT INTO coupons (code,discount,maximum_use,is_relative) VALUES ($1,$2,$3,$4) RETURNING *"
-            , [code, discount, maximumUse, isRelative]);
-        res.json(newCoupon.rows[0]);
-        //front end should have 4 things for add code
+        const { code, discount, maximum_use, is_relative } = req.body;
+        const couponExist = await pool.query("SELECT * FROM Coupons WHERE code = $1;", [code]);
+        if (couponExist.rowCount == 0) {
+            //validation in frontend needed
+            //discount positive 
+            //maximum positive  
+            //isRelative 0,1
+            const newCoupon = await pool.query("INSERT INTO coupons (code,discount,maximum_use,is_relative) VALUES ($1,$2,$3,$4) RETURNING *"
+                , [code, discount, maximum_use, is_relative]);
+            res.json(newCoupon.rows[0]);
+            console.log(`SUCCESSFUL INSERTION`);
+            //front end should have 4 things for add code
+        }
+        else {
+            console.log(`${code} is already added in the system`);
+        }
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.delete("/deleteCoupon", async (req, res) => {
+    try {
+        //frontend should call this to delete a coupon sending coupon name
+        const { code } = req.body;
+        const deleteCoupon = await pool.query("DELETE FROM Coupons WHERE code = $1 RETURNING *;", [code]);
+        if (deleteCoupon.rowCount == 0) {
+            res.send(`COUPON ALREADY ISN'T IN THE SYSTEM`);
+            console.log(`COUPON ALREADY ISN'T IN THE SYSTEM`);
+        }
+        else {
+            res.json(deleteCoupon.rows[0]);
+            console.log(`SUCCESSFUL DELETION`);
+        }
     } catch (err) {
         console.error(err.message);
     }
@@ -64,6 +105,90 @@ app.get("/getCoupons", async (req, res) => {
         const getCoupon = await pool.query("SELECT * FROM Coupons");
         res.json(getCoupon.rows);
         //front end should loop on all coupons and display them
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//wishlist
+
+app.post("/addWishlist/:id", async (req, res) => {
+    try {
+        const { user_id } = req.params;
+        const { book_id } = req.body; //why did we do this
+        //NEEDS VALIDATION AND REVISION
+        const addWishlist = await pool.query("INSERT INTO wish_list_item VALUES ($1,$2)", [user_id, book_id]);
+        res.json(addWishlist);
+
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//admin views drivers
+app.get("/drivers", async (req, res) => {
+    try {
+        const viewDrivers = await pool.query("SELECT * FROM driver, user WHERE user.type = 3");
+        res.json(viewDrivers.rows);
+        //front end should loop on all drivers and display them
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//admin views driver
+app.get("/drivers/:id", async (req, res) => {
+    try {
+        const { ssn } = req.params;
+        const viewDriver = await pool.query("SELECT * FROM driver WHERE ssn = $1", [ssn]);
+        res.json(viewDriver.rows[0]);
+        //front end should display one driver when click on him
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//admin views stores
+app.get("/stores", async (req, res) => {
+    try {
+        const viewStores = await pool.query("SELECT * FROM user WHERE type = 1;");
+        res.json(viewStores.rows);
+        //front end should loop on all stores and display them
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//admin views store
+app.get("/stores/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const viewStore = await pool.query("SELECT * FROM user WHERE type = 1 AND id = $1", [id]);
+        res.json(viewStore.rows[0]);
+        //front end should display one store when click on it
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//admin views users
+app.get("/users", async (req, res) => {
+    try {
+        const viewUsers = await pool.query("SELECT * FROM user WHERE type = 0;");
+        res.json(viewUsers.rows);
+        //front end should loop on all users and display them
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//admin views user
+app.get("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const viewUser = await pool.query("SELECT * FROM user WHERE type = 0 AND id = $1", [id]);
+        res.json(viewUser.rows[0]);
+        //front end should display one user when click on it
     } catch (err) {
         console.error(err.message);
     }
