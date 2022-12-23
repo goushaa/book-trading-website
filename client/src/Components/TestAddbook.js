a3352861ea978affaf25f5a0c124bff49d7da15b
goushaa <93336002+goushaa@users.noreply.github.com>
2022-12-24 00:55:39 +0200

    books now can upload image

--- client/src/Components/TestAddbook.js
+++ client/src/Components/TestAddbook.js
@@ -8,7 +8,6 @@ function TestAddbook() {
     const [genres, setGenres] = useState([]);
     const [languages, setLanguages] = useState([]);
     const [cities, setCities] = useState([]); //should be used in other page (wrote in here for practice)
-    console.log(cities); //should be used in other page (wrote in here for practice)
     //title, genre_id, isbn, author_name, language_id, purshace_price, version, description, image, user_id, count
     const [title, setTitle] = useState('');
     const [genre_id, setGenre] = useState('');
@@ -19,6 +18,9 @@ function TestAddbook() {
     const [version, setVersion] = useState('');
     const [description, setDescription] = useState('');
     const [image, setImage] = useState('');
+    const count = 1 //to be changed if store
+    const user_id = 2 //to be passed between pages
+    const [base64, settingBase64] = useState('')
     useEffect(() => {
         axios.get('http://localhost:5000/genres').then((res) => {
             setGenres(res.data);
@@ -37,12 +39,10 @@ function TestAddbook() {
     }
 
     function changeAuthor(e) {
-
         setAuthor(e.target.value);
     }
 
     function changePrice(e) {
-
         setPrice(e.target.value);
     }
 
@@ -67,7 +67,36 @@ function TestAddbook() {
     }
 
     function changeURL(e) {
-        setImage(e.target.value);
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        reader.onloadend = function () {
+
+            settingBase64(reader.result);
+        }
+        reader.readAsDataURL(file);
+    }
+
+
+
+    async function tryImage(e) {
+
+        var file = document.getElementById('imageInput');
+        var form = new FormData();
+        form.append("image", file.files[0])
+        var settings = {
+            "url": "https://api.imgbb.com/1/upload?key=bdea9a234b122ef7ef9abc17db08173e",
+            "method": "POST",
+            "timeout": 0,
+            "processData": false,
+            "mimeType": "multipart/form-data",
+            "contentType": false,
+            "data": form
+        };
+        let imgbb = await axios(settings);
+        setImage(imgbb.data.data.display_url);
+        axios.post('http://localhost:5000/addbook', { title, genre_id, isbn, author_name, language_id, purshace_price, version, description, image, user_id, count }).then(res => {
+            console.log(res.data); //add book with image upload (has some bugs)
+        }).catch(err => console.log(err))
     }
 
 
@@ -108,7 +137,7 @@ function TestAddbook() {
                     <select className='form-control'>
                         {
                             genres.map(genre => (
-                                <option key={genre.id}>{genre.name} </option>
+                                <option value={genre.id}>{genre.name} </option>
                             ))
                         }
                     </select>
@@ -121,7 +150,7 @@ function TestAddbook() {
                     <select className='form-control'>
                         {
                             languages.map(language => (
-                                <option key={language.id}>{language.name} </option>
+                                <option value={language.id}>{language.name} </option>
                             ))
                         }
                     </select>
@@ -132,12 +161,12 @@ function TestAddbook() {
                     <Form.Control type="text" placeholder="Enter book isbn" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword" onChange={changeURL}>
+                <Form.Group className="mb-3" onChange={changeURL}>
                     <Form.Label>Book Image</Form.Label>
-                    <Form.Control type="file" placeholder="Enter Image" />
+                    <Form.Control type="file" id='imageInput' placeholder="Enter Image" />
                 </Form.Group>
 
-                <Button variant="dark" >
+                <Button variant="dark" onClick={tryImage}>
                     Add Book
                 </Button>
             </Form>
