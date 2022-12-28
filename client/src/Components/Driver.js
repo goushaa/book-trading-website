d8876186bb7c1d2700ea8b13e670b537dc1d17d0
MustafaTarek77 <111199608+MustafaTarek77@users.noreply.github.com>
2022-12-28 04:26:08 +0200

    ay 7aga

--- client/src/Components/Driver.js
+++ client/src/Components/Driver.js
@@ -6,44 +6,61 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import "../CSS/prom_bg.css";
 import axios from "axios";
-
+import { useParams } from "react-router-dom";
 
 function Driver() {
-    return (
-        <Fragment>
-            <Navbar bg="dark" variant="dark" expand="lg">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <h3>Driver</h3>
-                    </Navbar.Brand>
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/driverssnfromdriverid", { id })
+      .then((res) => {
+        setssn(res.data.ssn);
+      })
+      .catch((err) => console.log(err));
 
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Item></Nav.Item>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Container className="Store_bg">
-                <Tabs defaultActiveKey="AddBook" id="justify-tab-example" className="mb-3" justify >
-                    <Tab eventKey="ViewAssignedOrders" title="View Assigned Orders">
+    axios
+      .get(`http://localhost:5000/ordersbycertaindriver/${ssn}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-                    </Tab>
-                    <Tab eventKey="ViewDeliveredOrders" title="View Delivered Orders">
+  let { id } = useParams();
+  const [ssn, setssn] = useState(0);
 
+  return (
+    <Fragment>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <h3>Driver</h3>
+          </Navbar.Brand>
 
-
-                    </Tab>
-                    <Tab eventKey="ViewWishlist" title="View Customers' Wishlists">
-
-
-
-                    </Tab>
-                </Tabs>
-            </Container>
-        </Fragment>
-    )
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Item></Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="Store_bg">
+        <Tabs
+          defaultActiveKey="AddBook"
+          id="justify-tab-example"
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="ViewAssignedOrders" title="View Assigned Orders"></Tab>
+          <Tab
+            eventKey="ViewDeliveredOrders"
+            title="View Delivered Orders"
+          ></Tab>
+          <Tab eventKey="ViewWishlist" title="View Customers' Wishlists"></Tab>
+        </Tabs>
+      </Container>
+    </Fragment>
+  );
 }
 
-export default Driver
+export default Driver;
